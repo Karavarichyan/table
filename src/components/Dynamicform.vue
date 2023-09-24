de9f2e5ca0709f49dbb1a6c0c04c48d93337f79e
Narek <narekkaravarichyan1910@gamil.com>
2023-09-25 01:54:07 +0300

    next

--- src/components/Dynamicform.vue
+++ src/components/Dynamicform.vue
@@ -4,22 +4,24 @@
       <div class="card-body">
         <div v-for="(item, index) in form" :key="index">
           <h3>Item {{ index + 1 }}</h3>
-          <div class="row">
-            <div class="col-sm-3">
-              <label>Name</label>
-              <input type="text" class="form-control" v-model="item.name">
+          <div class="form-row">
+            <div class="form-group col-md-4">
+              <label for="name">Name</label>
+              <input type="text" class="form-control" v-model="item.name" id="name">
             </div>
-            <div class="col-sm-3">
-              <label>Surname</label>
-              <input type="text" class="form-control" v-model="item.surname">
+            <div class="form-group col-md-4">
+              <label for="surname">Surname</label>
+              <input type="text" class="form-control" v-model="item.surname" id="surname">
             </div>
-            <div class="col-sm-3">
-              <label>Qualification</label>
-              <input type="text" class="form-control" v-model="item.qualification">
+            <div class="form-group col-md-3">
+              <label for="qualification">Qualification</label>
+              <input type="text" class="form-control" v-model="item.qualification" id="qualification">
             </div>
-            <div class="col-sm-2">
-              <button type="button" class="btn btn-danger btn-sm" @click="removeRow(index)">x</button>&nbsp;
-              <button type="button" class="btn btn-success btn-sm" @click="addRow">+</button>
+            <div class="form-group col-md-1">
+              <div class="add-remove-buttons">
+                <button type="button" class="btn btn-danger btn-sm" @click="removeRow(index)">x</button>&nbsp;
+                <button type="button" class="btn btn-success btn-sm" @click="addRow">+</button>
+              </div>
             </div>
           </div>
         </div>
@@ -27,15 +29,25 @@
       </div>
     </div>
   </div>
-  
-  <!-- List of previously entered forms -->
+
   <div class="container mt-4">
     <h2>List of Previously Entered Forms:</h2>
-    <ul>
-      <li v-for="(item, index) in savedForms" :key="index">
-        <span>Name: {{ item.name }}, Surname: {{ item.surname }}, Qualification: {{ item.qualification }}</span>
-      </li>
-    </ul>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Surname</th>
+          <th>Qualification</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in savedForms" :key="index">
+          <td>{{ item.name }}</td>
+          <td>{{ item.surname }}</td>
+          <td>{{ item.qualification }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -85,44 +97,97 @@ export default {
 }
 </script>
 
-
-<style>
+<style scoped>
 .container {
   margin-top: 20px;
 }
 
 .card {
-  width: 100%;
-  max-width: 600px;
+  max-width: 800px;
   margin: 0 auto;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
   padding: 20px;
 }
 
-.card-body {
-  display: flex;
-  flex-direction: column;
+.card-title {
+  font-size: 24px;
+  margin-bottom: 20px;
+  color: #333;
 }
 
-.row {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+.form-control {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
   margin-bottom: 10px;
-}
-
-.col-sm-3,
-.col-sm-2 {
-  flex: 1;
-  margin-right: 10px;
-}
-
-.col-sm-2 {
-  margin-right: 0;
-  flex: 0.5;
+  background-color: #f9f9f9;
+  color: #333;
 }
 
 .btn {
   margin-top: 10px;
+  font-size: 16px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background-color: #0056b3;
+}
+
+.table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+.table th {
+  background-color: #f0f0f0;
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: center;
+  color: #333;
+}
+
+.table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: center;
+  color: #333;
+}
+
+.add-remove-buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.add-remove-buttons button {
+  margin: 0 5px;
+}
+
+.error-message {
+  color: red;
+  font-size: 14px;
+  margin-top: 5px;
+}
+
+
+@media (max-width: 767px) {
+  .form-group {
+    margin-bottom: 10px;
+  }
+
+  .add-remove-buttons {
+    justify-content: space-between;
+  }
 }
 </style>
